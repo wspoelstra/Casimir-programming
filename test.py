a0f8f78b66e47170db35762ee64dd5b86cdbfe1f
Kasper <W.K.Spoelstra@tudelft.nl>
2021-01-25 14:07:27 +0100

    Added area calculation and took import pi outside the function definitions

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 print('Hello World')
+from math import pi
 
 def CircleCircum(R):
-	from math import pi
 	C = 2*pi*R
 	return C
 
+def Surf(R):
+	A = pi*R**2
+	return A
 
-
+print("Circle circumference = ", C)
+print("Circle surface area = ", A)
