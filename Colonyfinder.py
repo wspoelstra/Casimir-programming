010922d960104e21f905b8b039de665201c7f5d1
Kasper <W.K.Spoelstra@tudelft.nl>
2021-01-26 14:01:10 +0100

    Circle detection for individual plates now working

--- Colonyfinder.py
+++ Colonyfinder.py
@@ -1,13 +1,24 @@
+import timeit
+Elapsed = timeit.timeit()
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import scipy.ndimage as nd
 import cv2
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+img = cv2.imread('bacterial_colonies.jpg',0)
 
-image = mpimg.imread('bacterial_colonies.jpg')
-gray = rgb2gray(image)
-img = plt.imshow(gray, cmap='gray')
-plt.imsave('Outputimage.png', gray, cmap='gray')
+#plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1.5,100,param1=300,param2=100,minRadius=100,maxRadius=200)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+plt.imshow(cimg)
+print(Elapsed)
